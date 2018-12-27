d90d623536abce74170e1b4ac9dfa1994fc1434c
Kyle Desktop <kylespresser@gmail.com>
2018-12-27 14:01:19 -0500

    lecture 15

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose
+  .connect(
+    'mongodb://localhost/vidjot-dev',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
