03e869333a4a84546e63e76715e26079fc723627
Kyle Desktop <kylespresser@gmail.com>
2018-12-28 09:29:36 -0500

    lecture 17

--- vidjot/app.js
+++ vidjot/app.js
@@ -35,6 +35,11 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
 const port = 5000;
 
 app.listen(port, () => {
