fe3bc4654295afc42f4786764a81197fe03ce682
Kyle Desktop <kylespresser@gmail.com>
2018-12-28 15:22:15 -0500

    lecture 22

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -19,9 +20,12 @@ mongoose
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Body parser middleware
+// Body parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Method Override Middleware
+app.use(methodOverride('_method'));
 
 // Load Idea Model
 require('./models/Idea');
@@ -91,6 +95,20 @@ app.post('/ideas', (req, res) => {
       res.redirect('/ideas');
     });
   }
+});
+
+// Edit Form process
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save().then(idea => {
+      res.redirect('/ideas');
+    });
+  });
 });
 
 const port = 5000;
