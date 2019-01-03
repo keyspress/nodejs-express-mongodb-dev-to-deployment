f716db827a21386406694500bdd19c5b7b11795d
Kyle Desktop <kylespresser@gmail.com>
2019-01-03 13:29:59 -0500

    lecture 28

--- vidjot/routes/users.js
+++ vidjot/routes/users.js
@@ -12,4 +12,27 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
+// Resiter Form POST
+router.post('/register', (req, res) => {
+  let errors = [];
+
+  if (req.body.password !== req.body.password2) {
+    errors.push({ text: 'Passwords do not match' });
+  }
+  if (req.body.password < 4) {
+    errors.push({ text: 'Passord must be at least 4 characters' });
+  }
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send('passed');
+  }
+});
+
 module.exports = router;
