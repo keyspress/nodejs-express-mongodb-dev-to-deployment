5d843e0384cd68afe33342278085765d50eb70dd
Kyle Desktop <kylespresser@gmail.com>
2019-01-04 12:21:31 -0500

    lecture 42

--- storybooks/routes/auth.js
+++ storybooks/routes/auth.js
@@ -4,8 +4,15 @@ const passport = require('passport');
 
 router.get(
   '/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }),
-  (req, res) => {}
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
 );
 
 module.exports = router;
