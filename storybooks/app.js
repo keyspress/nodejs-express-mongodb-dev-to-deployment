158384bf899a07e4b351cf118ad81758bb2c4a6b
Kyle Desktop <kylespresser@gmail.com>
2019-01-04 14:04:00 -0500

    lecture 45

--- storybooks/app.js
+++ storybooks/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
 
 // Load User Model
@@ -32,6 +34,25 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Working');
+});
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
 // Use Routes
