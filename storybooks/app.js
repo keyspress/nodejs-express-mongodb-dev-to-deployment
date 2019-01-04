5e9e876f1d2ed7b62a443d11578bb4b0562aef01
Kyle Desktop <kylespresser@gmail.com>
2019-01-04 13:28:01 -0500

    lecture 43

--- storybooks/app.js
+++ storybooks/app.js
@@ -8,6 +8,23 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+// Mongoose Connect
+mongoose
+  .connect(
+    keys.mongoURI,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
 const app = express();
 
 app.get('/', (req, res) => {
