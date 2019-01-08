bd11eef60da45894322a661d4d2fb8ea89a34179
Kyle Desktop <kylespresser@gmail.com>
2019-01-08 13:07:25 -0500

    lecture 62

--- storybooks/routes/stories.js
+++ storybooks/routes/stories.js
@@ -101,4 +101,18 @@ router.delete('/:id', (req, res) => {
   });
 });
 
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({ _id: req.params.id }).then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    };
+    story.comments.unshift(newComment);
+    story.save().then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
+  });
+});
+
 module.exports = router;
